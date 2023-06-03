1ff650775bdbfbb171252b3ce073d0f0a60fe3a5
Andrea Olivos <andrea.olivos.b@gmail.com>
2023-06-03 09:30:01 -0400

    Add functions to edit and update products

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,7 @@ const prodColor = document.getElementById('prod-color');
 const prodPrice = document.getElementById('prod-price');
 const prodQuantity = document.getElementById('prod-quantity');
 const addButton = document.getElementById('add-button');
+const updateButton = document.getElementById('update-button');
 const productForm = document.getElementById('product-form');
 const tableBody = document.getElementById('table-body');
 
@@ -53,12 +54,12 @@ function listProducts () {
 ////////////////////////////////////////////////////////
 addButton.addEventListener('click', addProduct);
 
-function addProduct (e) {
+function addProduct (event) {
   // Prevent the button from resetting the page
-  e.preventDefault();
+  event.preventDefault();
 
   // Generate an ID / key for the new product
-  let productID = Date.now();
+  const productID = Date.now();
 
   // Create the new product with the data on the form
   const newProduct = {
@@ -83,15 +84,66 @@ function addProduct (e) {
 function deleteProduct (key) {
 
   // Get product from local storage
-  let product = JSON.parse(localStorage.getItem(key));
+  const product = JSON.parse(localStorage.getItem(key));
   // Create message to show on alert
-  let message = `¿De verdad quiere eliminar el producto "${product.name} ${product.color}"?`;
+  const message = `¿De verdad quiere eliminar el producto "${product.name} ${product.color}"?`;
 
   // Ask if user really wants to delete the product
   if (confirm(message)) {
     // Remove product from array and display updated list of products
     localStorage.removeItem(key);
     listProducts();
+  }
+
+}
+
+
+////////////////////////////////////////////////////////
+// Edit and update product in local storage
+////////////////////////////////////////////////////////
+
+// Fill the form with the data of the product the user wants to edit
+function editProduct (key) {
+
+  const product = JSON.parse(localStorage.getItem(key));
+  prodName.value = product.name;
+  prodColor.value = product.color;
+  prodPrice.value = product.price;
+  prodQuantity.value = product.quantity;
+
+  // Hide add button and display update button
+  addButton.classList.add('hide');
+  updateButton.classList.remove('hide');
+
+  // Add listener with update function and product key to the update button
+  updateButton.addEventListener('click', updateProduct(key));
+
+}
+
+// When user clicks the update button, update the product in local storage
+function updateProduct (key) {
+
+  return function (event) {
+    // Prevent the button from reloading the page
+    event.preventDefault();
+
+    const updatedProduct = {
+      name: prodName.value,
+      color: prodColor.value,
+      price: prodPrice.value,
+      quantity: prodQuantity.value
+    };
+
+    // Add new product to local storage
+    localStorage.setItem(key, JSON.stringify(updatedProduct));
+
+    // Show updated product list on table and reset form
+    listProducts();
+    productForm.reset();
+
+    // Hide update button and display add button
+    updateButton.classList.add('hide');
+    addButton.classList.remove('hide');
   }
 
 }
